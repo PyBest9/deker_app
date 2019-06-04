355c720d60d7efe5a3386a22b77c37b1301e26f7
PyBest9 <49530527+PyBest9@users.noreply.github.com>
2019-06-04 16:56:45 +0200

    Add files via upload

---
+++ deker_app/main.js
@@ -0,0 +1,254 @@
+function play1(){
+		var d = document.getElementById('play1').src = 'img/play-button.png'
+		var s = document.getElementById('pause1').src = 'img/pause1.png'
+	};
+	function pause1(){
+		var d = document.getElementById('play1').src = 'img/play-button (1).png'
+		var s = document.getElementById('pause1').src = 'img/pause2.png'
+
+	};
+	function play2(){
+		var d = document.getElementById('play2').src = 'img/play-button.png'
+		var s = document.getElementById('pause2').src = 'img/pause1.png'
+	};
+	function pause2(){
+		var d = document.getElementById('play2').src = 'img/play-button (1).png'
+		var s = document.getElementById('pause2').src = 'img/pause2.png'
+
+	};
+	function play3(){
+		var d = document.getElementById('play3').src = 'img/play-button.png'
+		var s = document.getElementById('pause3').src = 'img/pause1.png'
+	};
+	function pause3(){
+		var d = document.getElementById('play3').src = 'img/play-button (1).png'
+		var s = document.getElementById('pause3').src = 'img/pause2.png'
+
+	};
+	function play4(){
+		var d = document.getElementById('play4').src = 'img/play-button.png'
+		var s = document.getElementById('pause4').src = 'img/pause1.png'
+	};
+	function pause4(){
+		var d = document.getElementById('play4').src = 'img/play-button (1).png'
+		var s = document.getElementById('pause4').src = 'img/pause2.png'
+
+	};
+	function play5(){
+		var d = document.getElementById('play5').src = 'img/play-button.png'
+		var s = document.getElementById('pause5').src = 'img/pause1.png'
+	};
+	function pause5(){
+		var d = document.getElementById('play5').src = 'img/play-button (1).png'
+		var s = document.getElementById('pause5').src = 'img/pause2.png'
+
+	};
+		function play6(){
+		var d = document.getElementById('play6').src = 'img/play-button.png'
+		var s = document.getElementById('pause6').src = 'img/pause1.png'
+	};
+	function pause6(){
+		var d = document.getElementById('play6').src = 'img/play-button (1).png'
+		var s = document.getElementById('pause6').src = 'img/pause2.png'
+
+	};
+		function play7(){
+		var d = document.getElementById('play7').src = 'img/play-button.png'
+		var s = document.getElementById('pause7').src = 'img/pause1.png'
+	};
+	function pause7(){
+		var d = document.getElementById('play7').src = 'img/play-button (1).png'
+		var s = document.getElementById('pause7').src = 'img/pause2.png'
+	};
+
+  function play8(){
+    var d = document.getElementById('korsi_play').src = 'img/play-button.png'
+    var s = document.getElementById('korsi_pause').src = 'img/pause1.png'
+  };
+  function pause8(){
+    var d = document.getElementById('korsi_play').src = 'img/play-button (1).png'
+    var s = document.getElementById('korsi_pause').src = 'img/pause2.png'
+
+  };
+
+
+
+
+var a = document.getElementById("jebril_sabah");
+var b = document.getElementById("fares_sabah");
+var c = document.getElementById("afassi_sabah");
+var d = document.getElementById("kotami");
+var e = document.getElementById("jebril_massa");
+var f = document.getElementById("fares_massa");
+var i = document.getElementById("afassi_massa");
+var m = document.getElementById("korsi");
+
+
+
+function playAudio1() { 
+  a.play(); 
+  b.pause();
+  c.pause();
+  d.pause();
+  e.pause();
+  f.pause();
+  i.pause();
+
+pause2();
+pause3();
+pause4();
+pause5();
+pause6();
+pause7();
+} 
+
+function pauseAudio1() { 
+  a.pause(); 
+} 
+function playAudio2() { 
+  b.play();
+  a.pause(); 
+  c.pause();
+  d.pause();
+  e.pause();
+  f.pause();
+  i.pause(); 
+
+pause1();
+pause3();
+pause4();
+pause5();
+pause6();
+pause7();
+} 
+
+function pauseAudio2() { 
+  b.pause(); 
+} 
+function playAudio3() { 
+  c.play(); 
+  b.pause();
+  a.pause();
+  d.pause();
+  e.pause();
+  f.pause();
+  i.pause();
+
+pause1();
+pause2();
+pause4();
+pause5();
+pause6();
+pause7();
+} 
+
+function pauseAudio3() { 
+  c.pause(); 
+} 
+function playAudio4() { 
+  d.play();  
+  b.pause();
+  c.pause();
+  a.pause();
+  e.pause();
+  f.pause();
+  i.pause();
+
+pause1();
+pause2();
+pause3();
+pause5();
+pause6();
+pause7();
+
+} 
+
+function pauseAudio4() { 
+  d.pause(); 
+} 
+function playAudio5() { 
+  e.play(); 
+  b.pause();
+  c.pause();
+  d.pause();
+  a.pause();
+  f.pause();
+  i.pause();
+
+pause1();
+pause2();
+pause3();
+pause4();
+pause6();
+pause7();
+
+} 
+
+function pauseAudio5() { 
+  e.pause(); 
+} 
+function playAudio6() { 
+  f.play(); 
+  b.pause();
+  c.pause();
+  d.pause();
+  e.pause();
+  a.pause();
+  i.pause();
+
+
+pause1();
+pause2();
+pause3();
+pause4();
+pause5();
+pause7();
+} 
+
+function pauseAudio6() { 
+  f.pause(); 
+} 
+function playAudio7() { 
+  i.play(); 
+  b.pause();
+  c.pause();
+  d.pause();
+  e.pause();
+  f.pause();
+  a.pause();
+
+pause1();
+pause2();
+pause3();
+pause4();
+pause5();
+pause6();
+} 
+
+function pauseAudio7() { 
+  i.pause(); 
+} 
+function playAudio8() { 
+  m.play(); 
+  b.pause();
+  c.pause();
+  d.pause();
+  e.pause();
+  f.pause();
+  a.pause();
+  i.pause();
+
+
+pause1();
+pause2();
+pause3();
+pause4();
+pause5();
+pause6();
+pause7();
+} 
+
+function pauseAudio8() { 
+  m.pause(); 
+} 
+
